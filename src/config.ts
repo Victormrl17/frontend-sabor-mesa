26da9a074aa261c8b66492b760de78ec77082ba2
Victor Ramirez <106178464+Victormrl17@users.noreply.github.com>
2024-12-10 13:58:12 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL =
   import.meta.env.VITE_API_URL || 'http://localhost:3000'
-export const API_USERS_URL = `${API_BASE_URL}/api/users`
-export const API_RESTAURANTS_URL = `${API_BASE_URL}/api/restaurants`
-export const API_BOOKINGS_URL = `${API_BASE_URL}/api/bookings`
-export const API_COMMENTS_URL = `${API_BASE_URL}/api/comments`
+export const API_USERS_URL = `${API_BASE_URL}/api`
+export const API_RESTAURANTS_URL = `${API_BASE_URL}/api`
+export const API_BOOKINGS_URL = `${API_BASE_URL}/api`
+export const API_COMMENTS_URL = `${API_BASE_URL}/api`
